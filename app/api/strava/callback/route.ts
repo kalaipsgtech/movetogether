fd0e6fa785b231fe24410e11f06428aa060039c7
kalaipsgtech <kalaipsgtech@gmail.com>
2026-07-30 21:05:14 +0530

    Add Strava Callback

--- app/api/strava/callback/route.ts
+++ app/api/strava/callback/route.ts
@@ -1,1 +1,12 @@
+import { NextResponse } from "next/server";
 
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const code = searchParams.get("code");
+
+  return NextResponse.json({
+    success: true,
+    stravaCode: code,
+  });
+}
