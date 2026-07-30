9fd4b78f92fabb303fed0f3ffb92ee20685c64d9
kalaipsgtech <kalaipsgtech@gmail.com>
2026-07-30 20:48:49 +0530

    Connect Strava button

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
+"use client";
+
 export default function Home() {
+  const connectStrava = () => {
+    const clientId = process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID;
+    const redirectUri = process.env.NEXT_PUBLIC_REDIRECT_URI;
+
+    const authUrl =
+      https://www.strava.com/oauth/authorize +
+      ?client_id=${clientId} +
+      &response_type=code +
+      &redirect_uri=${redirectUri} +
+      &approval_prompt=force +
+      &scope=read,activity:read_all;
+
+    window.location.href = authUrl;
+  };
+
   return (
     <main
       style={{
@@ -16,10 +33,12 @@ export default function Home() {
       </p>
 
       <button
+        onClick={connectStrava}
         style={{
           padding: "12px 24px",
           fontSize: "18px",
           marginTop: "20px",
+          cursor: "pointer",
         }}
       >
         Connect Strava
