21dc6dfb5ab3eea53f2666b9478c1061df4884df
kalaipsgtech <kalaipsgtech@gmail.com>
2026-07-30 21:09:51 +0530

    Exchange code for token

--- app/api/strava/callback/route.ts
+++ app/api/strava/callback/route.ts
@@ -5,8 +5,23 @@ export async function GET(request: Request) {
 
   const code = searchParams.get("code");
 
-  return NextResponse.json({
-    success: true,
-    stravaCode: code,
-  });
+  const response = await fetch(
+    "https://www.strava.com/oauth/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        client_id: process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID,
+        client_secret: process.env.STRAVA_CLIENT_SECRET,
+        code,
+        grant_type: "authorization_code",
+      }),
+    }
+  );
+
+  const data = await response.json();
+
+  return NextResponse.json(data);
 }
