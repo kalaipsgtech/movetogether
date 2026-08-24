4117a9b760016a5be99ac0f6c104c9a8ca656304
kalaipsgtech <kalaipsgtech@gmail.com>
2026-08-24 15:10:27 +0530

    Fix Link components in BottomNavigation

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -7,23 +7,24 @@ export default function BottomNavigation() {
     <div className="fixed bottom-0 left-0 right-0 bg-slate-900 border-t border-slate-700 z-50">
       <div className="flex justify-around py-3 text-white">
 
-        /points-dashboard
-         <Link href  🏆 Leaderboard
+        <Link href="/points-dashboard">
+          🏆 Leaderboard
         </Link>
 
-        /streaks
-         <Link href  🔥 Streaks
+       <Link href="/streaks">
+          🔥 Streaks
         </Link>
 
-        <Link href       📈 Stats
+       <Link href="/stats">
+          📈 Stats
         </Link>
 
-        /family
-        <Link href   👨‍👩‍👧‍👦 Family
+       <Link href="/family">
+          👨‍👩‍👧‍👦 Family
         </Link>
 
-        /challenge
-         <Link href  🏅 Challenge
+       <Link href="/challenge">
+          🏅 Challenge
         </Link>
 
       </div>
