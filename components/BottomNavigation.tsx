f7e0999a3505053f26a7dfea772ddb0d21be7b2c
kalaipsgtech <kalaipsgtech@gmail.com>
2026-08-24 14:45:21 +0530

    Update BottomNavigation links to include paths

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -7,23 +7,23 @@ export default function BottomNavigation() {
     <div className="fixed bottom-0 left-0 right-0 bg-slate-900 border-t border-slate-700 z-50">
       <div className="flex justify-around py-3 text-white">
 
-        <Link href="/">
+        /points-dashboard
           🏆 Leaderboard
         </Link>
 
-        <Link href="/">
+        /streaks
           🔥 Streaks
         </Link>
 
-        <Link href="/">
+        /stats
           📈 Stats
         </Link>
 
-        <Link href="/">
+        /family
           👨‍👩‍👧‍👦 Family
         </Link>
 
-        <Link href="/">
+        /challenge
           🏅 Challenge
         </Link>
 
